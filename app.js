110d85d544e1a2f3a9dc8258282decb6c7ad03a3
Jhosan7 <jhosan@gmail.com>
2022-06-18 16:35:48 +0200

    update

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const bodyparser = require("body-parser");
 const cors = require('cors');
 const verify = require('./verify');
 
+const axios = require('axios');
+
 const users = require('./users.json');
 const properties = require('./properties.json');
 const cars = require('./cars.json');
 
 const app = express();
+
+const crypto = require('crypto');
+const base64url = require("base64url");
+const randomstring = require("randomstring");
+const url = require('url');
 
 app.use(cors());
 app.use(router);
@@ -85,6 +92,136 @@ router.route('/car/:id')
         res.status(400).jsonp({ error: 400, msg: 'car not found' });
     });
 
+/* router.route('/')
+    .get((req, res) => {
+        console.log('-->', req.query.state);
+        if (req.query.code) {
+            const params = new URLSearchParams();
+            params.append('grant_type', 'authorization_code');
+            params.append('code', req.query.code);
+            params.append('client_id', '237n2282ge6ot4tr1ip15eg0j9');
+            params.append('redirect_uri', 'http://localhost:8080');
+
+            const buff = new Buffer.from('237n2282ge6ot4tr1ip15eg0j9:14qr47hdpp8roajnpksedh1ft5j5bp6c4j9oir4ubothdsjp3o3p');
+            const base64data = buff.toString('base64');
+            axios.post('https://login.imjose.app/oauth2/token', params, {
+                headers: {
+                    Authorization: `Basic ${base64data}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .then(response => {
+                    res.status(200).jsonp({ statusCode: 200, data: response.data });
+                })
+                .catch(error => {
+                    console.log('--->', error.response)
+                    if (error.response?.data?.error === 'invalid_grant') return res.status(400).jsonp({ statusCode: 401, data: 'unauthorized' });
+                    console.log(error.response?.data);
+                    return res.status(400).jsonp({ statusCode: 200, data: 'internal error' });
+                });
+        }
+        else res.status(400).jsonp({ statusCode: 400, msg: 'bad request' });
+    }); */
+
+
+    global.state = null;
+    global.codeVerifier = null;
+
+    const clientId = '778b93ip0e9qp5k07ftjojnidb';
+
+    app.disable('etag');
+
+    router.route('/login')
+    .get((req, res) => {
+        state = randomstring.generate(128);
+        codeVerifier = randomstring.generate(128);
+
+        console.log('-222-->', state, codeVerifier);
+        
+        // client_id: 778b93ip0e9qp5k07ftjojnidb
+        return res.redirect(302, url.format({
+            protocol: 'https',
+            host: 'login.imjose.app',
+            pathname: '/oauth2/authorize',
+            query: {
+                response_type: 'code',
+                client_id: clientId,
+                redirect_uri: 'http://localhost:8080/auth',
+                // scope: 'openid',
+                state: state,
+                code_challenge: base64url.fromBase64(crypto.createHash('sha256').update(codeVerifier).digest('base64')),
+                code_challenge_method: 'S256'
+            },
+        }));
+        // res.status(200).jsonp({ statusCode: 200, data: 'ey' });
+    });
+
+router.route('/auth')
+    .get((req, res) => {
+        if (req.query.code && req.query.state && req.query.state === state) {
+            const params = new URLSearchParams();
+            params.append('grant_type', 'authorization_code');
+            params.append('code', req.query.code);
+            params.append('client_id', clientId);
+            params.append('redirect_uri', 'http://localhost:8080/auth');
+            params.append('code_verifier', codeVerifier);
+
+            const buff = new Buffer.from(`${clientId}:${req.query.code}`);
+            const base64data = buff.toString('base64');
+            axios.post('https://login.imjose.app/oauth2/token', params, {
+                headers: {
+                    // Authorization: `Basic ${base64data}:`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .then(response => {
+                    res.status(200).jsonp({ statusCode: 200, data: response.data });
+                })
+                .catch(error => {
+                    console.log(error.response?.data);
+                    if (error.response?.data?.error === 'invalid_grant') return res.status(400).jsonp({ statusCode: 401, data: 'unauthorized' });
+                    return res.status(400).jsonp({ statusCode: 200, data: 'internal error' });
+                });
+        }
+        else res.status(400).jsonp({ statusCode: 400, msg: 'bad request' });
+    });
+
+    router.route('/')
+        .get((req, res) => {
+            res.status(200).jsonp({ statusCode: 200, data: 'ok' });
+        });
+
+/* router.route('/')
+    .get((req, res) => {
+        console.log('-->', req.query.state);
+        if (req.query.code) {
+            const params = new URLSearchParams();
+            params.append('grant_type', 'authorization_code');
+            params.append('code', req.query.code);
+            params.append('client_id', '237n2282ge6ot4tr1ip15eg0j9');
+            params.append('redirect_uri', 'http://localhost:8080');
+
+            const buff = new Buffer.from('237n2282ge6ot4tr1ip15eg0j9:14qr47hdpp8roajnpksedh1ft5j5bp6c4j9oir4ubothdsjp3o3p');
+            const base64data = buff.toString('base64');
+            axios.post('https://login.imjose.app/oauth2/token', params, {
+                headers: {
+                    Authorization: `Basic ${base64data}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .then(response => {
+                    res.status(200).jsonp({ statusCode: 200, data: response.data });
+                })
+                .catch(error => {
+                    console.log('--->', error.response)
+                    if (error.response?.data?.error === 'invalid_grant') return res.status(400).jsonp({ statusCode: 401, data: 'unauthorized' });
+                    console.log(error.response?.data);
+                    return res.status(400).jsonp({ statusCode: 200, data: 'internal error' });
+                });
+        }
+        else res.status(400).jsonp({ statusCode: 400, msg: 'bad request' });
+    }); */
+
 
 
 // Error 404
@@ -93,7 +230,7 @@ router.use(function (req, res, next) {
 });
 
 
-app.listen(8001, () => {
-  console.log('Listening... http://localhost:8001');
+app.listen(8080, () => {
+  console.log('Listening... http://localhost:8080');
 });
 
